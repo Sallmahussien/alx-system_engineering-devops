fd64bf567a6ff16ce3414256ea65642c5023b74d
Salma Hussien <salmaa.hussien1@gmail.com>
2023-11-07 13:17:33 +0200

    Add task 2

---
+++ 0x16-api_advanced/2-recurse.py
@@ -0,0 +1,28 @@
+#!/usr/bin/python3
+"""Implement recurse function"""
+
+import requests
+
+
+def recurse(subreddit, hot_list=[], after=None):
+    """Returns list of hot articles titles"""
+    url = 'https://www.reddit.com/r/{}/hot.json'.format(subreddit)
+    params = {'limit': 100, 'after': after}
+    headers = {'User-Agent': 'custom user-agent'}
+
+    response = requests.get(url,
+                            params=params,
+                            headers=headers,
+                            allow_redirects=False)
+
+    if response.status_code == 404:
+        return None
+
+    data = response.json().get('data', {})
+    for post in data.get('children', []):
+        hot_list.append(post)
+
+    if data.get('after'):
+        return recurse(subreddit, hot_list, data.get('after'))
+    else:
+        return hot_list

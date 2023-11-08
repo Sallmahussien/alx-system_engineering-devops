fd7a5207d2024aad976235298a101c58f8201128
Salma Hussien <salmaa.hussien1@gmail.com>
2023-11-08 07:06:50 +0200

    some fixes

--- 0x16-api_advanced/100-count.py
+++ 0x16-api_advanced/100-count.py
@@ -22,31 +22,38 @@ def count_words(subreddit, word_list, after=None, count_dict=None):
                             headers=headers,
                             allow_redirects=False)
 
-    if response.status_code == 200:
-        data = response.json().get('data', {})
-        for post in data.get('children', []):
-            title = post.get('data', {}).get('title', '').lower().split()
+    try:
+        results = response.json()
+        if response.status_code == 404:
+            raise Exception
+    except Exception:
+        print()
+        return
 
-            for key in count_dict.keys():
-                if key in title:
-                    times = len([t for t in title if t == key.lower()])
-                    count_dict[key] += times
+    data = response.json().get('data', {})
+    for post in data.get('children', []):
+        title = post.get('data', {}).get('title', '').lower().split()
 
-        if data.get('after'):
-            return count_words(subreddit, word_list, data.get('after'), count_dict)
+        for key in count_dict.keys():
+            if key in title:
+                times = len([t for t in title if t == key.lower()])
+                count_dict[key] += times
 
-        else:
-            if len(word_list) > len(count_dict.keys()):
-                temp_dict = count_dict.copy()
-                for word in word_list:
-                    word_in_lowecase = word.lower()
-                    if word not in count_dict and word_in_lowecase in count_dict:
-                        count_dict[word_in_lowecase] += temp_dict[word_in_lowecase]
+    if data.get('after'):
+        return count_words(subreddit, word_list, data.get('after'), count_dict)
 
-            count_dict = dict(sorted(count_dict.items(),
-                                    key=lambda item: (item[1],
-                                                    item[0].lower()),
-                                    reverse=True))
+    else:
+        if len(word_list) > len(count_dict.keys()):
+            temp_dict = count_dict.copy()
+            for word in word_list:
+                word_in_lowecase = word.lower()
+                if word not in count_dict and word_in_lowecase in count_dict:
+                    count_dict[word_in_lowecase] += temp_dict[word_in_lowecase]
 
-            [print('{}: {}'.format(key, value))
-            for key, value in count_dict.items() if value > 0]
+        count_dict = dict(sorted(count_dict.items(),
+                                 key=lambda item: (item[1],
+                                                   item[0].lower()),
+                                 reverse=True))
+
+        [print('{}: {}'.format(key, value))
+         for key, value in count_dict.items() if value > 0]
